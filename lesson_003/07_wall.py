0d8464bf68f78a175d8c0e8ca9764cdb965899d5
Mikhail <rymv@yandex.ru>
2021-02-28 10:28:17 +0300

    lesson 003 исправления TODO 07

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -20,7 +20,7 @@ import simple_draw as sd
 #  .
 #  С циклом "for" вам будет проще.
 
-# TODO Переделал c возможностью изменения параметров экрана и кирпича.
+#  Переделал c возможностью изменения параметров экрана и кирпича.
 #  Я не совсем понял, какой тип данных возвращает sd.get_point(100, 50) и как мы к ним обращаемся.
 #  return Point(x=x, y=y) ???
 #  BRICK_SIZE - что это? - это был пример создания константы
@@ -53,30 +53,31 @@ import simple_draw as sd
 # sd.clear_screen()
 
 
-SCREEN_WIDTH = 1020
-SCREEN_HEIGHT = 630
-sd.resolution = (SCREEN_WIDTH, SCREEN_HEIGHT)
-BRICK_SIZE = sd.get_point(75, 30)
-number_rows = SCREEN_HEIGHT // BRICK_SIZE.y + 1
-number_bricks = SCREEN_WIDTH // BRICK_SIZE.x + 1
-y_start = 0
-y_end = BRICK_SIZE.y
-for rows in range(1, number_rows + 1):
-    if rows % 2 == 0:
-        x_start = 0
-        x_end = BRICK_SIZE.x
-    else:
-        x_start = -BRICK_SIZE.x / 2
-        x_end = BRICK_SIZE.x / 2
-    for brick in range(1, number_bricks + 1):
-        left_bottom = sd.get_point(x_start, y_start)
-        right_top = sd.get_point(x_end, y_end)
-        sd.rectangle(left_bottom, right_top, color=sd.COLOR_YELLOW, width=1)
-        x_start += BRICK_SIZE.x
-        x_end += BRICK_SIZE.x
-    y_start += BRICK_SIZE.y
-    y_end += BRICK_SIZE.y
-# TODO Удобнее сразу на ходу рассчитывать данные
+# SCREEN_WIDTH = 1020
+# SCREEN_HEIGHT = 630
+# sd.resolution = (SCREEN_WIDTH, SCREEN_HEIGHT)
+# BRICK_SIZE = sd.get_point(75, 30)
+# number_rows = SCREEN_HEIGHT // BRICK_SIZE.y + 1
+# number_bricks = SCREEN_WIDTH // BRICK_SIZE.x + 1
+# y_start = 0
+# y_end = BRICK_SIZE.y
+# for rows in range(1, number_rows + 1):
+#     if rows % 2 == 0:
+#         x_start = 0
+#         x_end = BRICK_SIZE.x
+#     else:
+#         x_start = -BRICK_SIZE.x / 2
+#         x_end = BRICK_SIZE.x / 2
+#     for brick in range(1, number_bricks + 1):
+#         left_bottom = sd.get_point(x_start, y_start)
+#         right_top = sd.get_point(x_end, y_end)
+#         sd.rectangle(left_bottom, right_top, color=sd.COLOR_YELLOW, width=1)
+#         x_start += BRICK_SIZE.x
+#         x_end += BRICK_SIZE.x
+#     y_start += BRICK_SIZE.y
+#     y_end += BRICK_SIZE.y
+
+#  Удобнее сразу на ходу рассчитывать данные
 #  i = 0 # Счетчик рядов
 #  # Циклы лучше взять в диапазоне экрана, а шагом будет размер кирпича
 #  for y in range(0, sd.resolution[1] + 1, BRICK_SIZE.y):
@@ -90,6 +91,22 @@ for rows in range(1, number_rows + 1):
 #          left_bottom_brick = ...
 #          right_top_brick = ...
 #          рисуем кирпич
+
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 600
+sd.resolution = (SCREEN_WIDTH, SCREEN_HEIGHT)
+BRICK_SIZE = sd.get_point(100, 50)
+row = 0
+for y in range(0, sd.resolution[1] + 1, BRICK_SIZE.y):
+    row += 1
+    if row % 2 == 0:
+        x_start = 0
+    else:
+        x_start = -BRICK_SIZE.x // 2
+    for x in range(x_start, sd.resolution[0] + 1, BRICK_SIZE.x):
+        left_bottom = sd.get_point(x, y)
+        right_top = sd.get_point(x + BRICK_SIZE.x, y + BRICK_SIZE.y)
+        sd.rectangle(left_bottom, right_top, color=sd.COLOR_YELLOW, width=1)
 
 # Подсказки:
 #  Для отрисовки кирпича использовать функцию rectangle
