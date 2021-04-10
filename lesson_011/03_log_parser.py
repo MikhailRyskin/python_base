4907fbd8ba0ddd113ffcc42990ccd4605651e2b4
Mikhail <rymv@yandex.ru>
2021-04-10 14:37:39 +0300

    lesson 011 3

--- lesson_011/03_log_parser.py
+++ lesson_011/03_log_parser.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # На основе своего кода из lesson_009/02_log_parser.py напишите итератор (или генератор)
-# котрый читает исходный файл events.txt и выдает число событий NOK за каждую минуту
+# который читает исходный файл events.txt и выдает число событий NOK за каждую минуту
 # <время> <число повторений>
 #
 # пример использования:
@@ -14,4 +14,40 @@
 #
 # [2018-05-17 01:57] 1234
 
-# TODO здесь ваш код
+
+class LogParser:
+
+    def __init__(self, file_name):
+        in_file = open(file_name, 'r', encoding='utf8')
+        self.file = in_file.readlines()
+        in_file.close()
+        self.i = 0
+
+    def __iter__(self):
+        self.i = 0
+        return self
+
+    def __next__(self):
+        events_count = 0
+        while self.i < len(self.file):
+            line = self.file[self.i].rstrip()
+            current_line = line[1:17]
+            if line.endswith('NOK'):
+                events_count += 1
+            self.i += 1
+            if self.i == len(self.file):
+                if events_count:
+                    return current_line, events_count
+            else:
+                next_line = self.file[self.i][1:17]
+                if next_line != current_line:
+                    if events_count:
+                        return current_line, events_count
+        raise StopIteration
+
+
+my_file_name = 'events.txt'
+
+grouped_events = LogParser(my_file_name)
+for group_time, event_count in grouped_events:
+    print(f'[{group_time}] {event_count}')
