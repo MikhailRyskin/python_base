92ca9ea8d384e5593170d2e6f9e722d97f9ad932
Mikhail <rymv@yandex.ru>
2021-05-01 13:18:51 +0300

    lesson 013

--- lesson_013/01_ticket.py
+++ lesson_013/01_ticket.py
@@ -7,10 +7,28 @@
 # Шаблон взять в файле lesson_013/images/ticket_template.png
 # Пример заполнения lesson_013/images/ticket_sample.png
 # Подходящий шрифт искать на сайте ofont.ru
+import os
+from PIL import Image, ImageDraw, ImageFont, ImageColor
+import argparse
 
-def make_ticket(fio, from_, to, date):
-    # TODO здесь ваш код
-    pass
+
+def make_ticket(fio, from_, to, date, out_path=None):
+    font_path = os.path.join('fonts', 'Arial_Cyr.ttf')
+    template = 'ticket_template.png'
+    im = Image.open(template)
+    draw = ImageDraw.Draw(im)
+    font = ImageFont.truetype(font_path, size=14)
+    draw.text((45, 125), fio, font=font, fill=ImageColor.colormap['black'])
+    draw.text((45, 195), from_, font=font, fill=ImageColor.colormap['black'])
+    draw.text((45, 260), to, font=font, fill=ImageColor.colormap['black'])
+    draw.text((285, 260), date, font=font, fill=ImageColor.colormap['black'])
+    out_path = out_path if out_path else 'ticket.png'
+    im.save(out_path)
+    print(f'Билет сохранён как {out_path}')
+
+
+# make_ticket('ИВАНОВ Т.П.', 'САНКТ-ПЕТЕРБУРГ', 'МОСКВА', '01.05')
+
 
 # Усложненное задание (делать по желанию).
 # Написать консольный скрипт c помощью встроенного python-модуля argparse.
@@ -19,5 +37,14 @@ def make_ticket(fio, from_, to, date):
 #   --from - обязательный, откуда летим.
 #   --to - обязательный, куда летим.
 #   --date - обязательный, когда летим.
-#   --save_to - необязательный, путь для сохранения заполненнего билета.
+#   --save_to - необязательный, путь для сохранения заполненнoго билета.
 # и заполнять билет.
+
+parser = argparse.ArgumentParser(description='Make ticket')
+parser.add_argument('fio', type=str, help='FIO')
+parser.add_argument('from_', type=str, help='FROM')
+parser.add_argument('to', type=str, help='TO')
+parser.add_argument('data', type=str, help='DATA')
+parser.add_argument('--save_to', type=str, default='ticket.png', help='--save_to (default: ticket.png)')
+args = parser.parse_args()
+make_ticket(fio=args.fio, from_=args.from_, to=args.to, date=args.data, out_path=args.save_to)
