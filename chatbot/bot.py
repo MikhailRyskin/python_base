9ebde5ef2fdc5624e7a15c47d2a9745b89452f1b
Mikhail <rymv@yandex.ru>
2021-05-06 12:38:11 +0300

    chatbot lesson 013 TODO

--- chatbot/bot.py
+++ chatbot/bot.py
@@ -2,7 +2,7 @@
 import random
 from my_token import _token
 import vk_api
-from vk_api import bot_longpoll
+import vk_api.bot_longpoll
 
 
 my_group_id = 204336937
@@ -15,7 +15,7 @@ class Bot:
         self.vk = vk_api.VkApi(token=self.token)
         #  без "from vk_api import bot_longpoll" в  vk_api.bot_longpoll...  bot_longpoll не определяется. Почему?
         #  Разве недостаточно import vk_api?
-        # TODO Нет не достаточно,
+        #   Нет не достаточно,
         #  делая "import vk_api" вы попадаете в __init__.py пакета "vk_api" где нет ссылки на "bot_longpoll"
         #  А вам нужен модуль, который находится в этом пакете "bot_longpoll"
         #  И нужно указывать  import vk_api.bot_longpoll либо сразу класс from vk_api.bot_longpoll import VkBotLongPoll
@@ -35,7 +35,7 @@ class Bot:
         if event.type == vk_api.bot_longpoll.VkBotEventType.MESSAGE_NEW:
             # print(event.object)
             #  в видео используют event.object.text и event.object.peer_id, у меня это не работает. Почему?
-            # TODO Библиотека за это время немного изменилась и ключи соответственно тоже
+            # Библиотека за это время немного изменилась и ключи соответственно тоже
             print(event.object['message']['text'])
             self.api.messages.send(message='это эхо:' + event.object['message']['text'],
                                    random_id=random.randint(0, 2**20),
