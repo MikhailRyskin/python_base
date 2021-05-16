b4d39b924ca7942eda76494317673e43589af3ad
Mikhail <rymv@yandex.ru>
2021-05-16 18:13:01 +0300

    lesson 014 бот 2 части

--- chatbot/bot.py
+++ chatbot/bot.py
@@ -1,49 +1,73 @@
 # -*- coding: utf-8 -*-
 import random
-from my_token import _token
+import logging
+
 import vk_api
-import vk_api.bot_longpoll
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+
+try:
+    import settings
+except ImportError:
+    exit('Do cp settings.py.default settings.py and set token')
 
 
-my_group_id = 204336937
+log = logging.getLogger('bot')
+
+
+def configure_logging():
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(logging.Formatter('%(levelname)s %(message)s'))
+    log.addHandler(stream_handler)
+    stream_handler.setLevel(logging.INFO)
+
+    file_handler = logging.FileHandler(filename='bot.log', encoding='UTF-8')
+    file_handler.setFormatter(logging.Formatter(fmt='%(asctime)s %(levelname)s %(message)s', datefmt='%d-%m-%y %H:%M'))
+    log.addHandler(file_handler)
+    file_handler.setLevel(logging.DEBUG)
+
+    log.setLevel(logging.DEBUG)
 
 
 class Bot:
+    """
+    Echo bot для vk.com
+    Use Python 3.8
+    """
     def __init__(self, group_id, token):
+        """
+        :param group_id: group id из группы в vk
+        :param token: секретный токен группы в vk
+        """
         self.group_id = group_id
         self.token = token
         self.vk = vk_api.VkApi(token=self.token)
-        #  без "from vk_api import bot_longpoll" в  vk_api.bot_longpoll...  bot_longpoll не определяется. Почему?
-        #  Разве недостаточно import vk_api?
-        #   Нет не достаточно,
-        #  делая "import vk_api" вы попадаете в __init__.py пакета "vk_api" где нет ссылки на "bot_longpoll"
-        #  А вам нужен модуль, который находится в этом пакете "bot_longpoll"
-        #  И нужно указывать  import vk_api.bot_longpoll либо сразу класс from vk_api.bot_longpoll import VkBotLongPoll
-        #  Это тоже самое что если бы в прошлом модуле импортировали не "from PIL", а просто "import PIL"
-        self.long_poller = vk_api.bot_longpoll.VkBotLongPoll(vk=self.vk, group_id=self.group_id)
+        self.long_poller = VkBotLongPoll(vk=self.vk, group_id=self.group_id)
         self.api = self.vk.get_api()
 
     def run(self):
+        """Запуск бота."""
         for event in self.long_poller.listen():
-            print('получено событие')
             try:
                 self.on_event(event)
-            except Exception as exp:
-                print(exp)
+            except Exception:
+                log.exception('ошибка в обработке события')
 
     def on_event(self, event):
-        if event.type == vk_api.bot_longpoll.VkBotEventType.MESSAGE_NEW:
-            # print(event.object)
-            #  в видео используют event.object.text и event.object.peer_id, у меня это не работает. Почему?
-            # Библиотека за это время немного изменилась и ключи соответственно тоже
-            print(event.object['message']['text'])
-            self.api.messages.send(message='это эхо:' + event.object['message']['text'],
+        """
+        Отправляет сообщение назад, если это текст
+        :param event: VkBotMessageEvent object
+        :return:
+        """
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            log.debug('отправляем сообщение назад')
+            self.api.messages.send(message='это эхо: ' + event.object['message']['text'],
                                    random_id=random.randint(0, 2**20),
                                    peer_id=event.object['message']['peer_id'])
         else:
-            print('мы пока не умеем обрабатывать такие события', event.type)
+            log.info('мы пока не умеем обрабатывать такие события %s', event.type)
 
 
 if __name__ == '__main__':
-    bot = Bot(my_group_id, _token)
+    configure_logging()
+    bot = Bot(settings.GROUP_ID, settings.TOKEN)
     bot.run()
