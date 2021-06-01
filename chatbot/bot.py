f1809d7aa3715a7cbdfd3beae01b4e803d08d5dc
Mikhail <rymv@yandex.ru>
2021-06-01 12:47:39 +0300

    lesson 015 бот начало

--- chatbot/bot.py
+++ chatbot/bot.py
@@ -5,6 +5,8 @@ import logging
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
+from chatbot import handlers
+
 try:
     import settings
 except ImportError:
@@ -28,10 +30,23 @@ def configure_logging():
     log.setLevel(logging.DEBUG)
 
 
+class UserState:
+    """ Состояние пользователя внутри сценария."""
+    def __init__(self, scenario_name, step_name, context=None):
+        self.scenario_name = scenario_name
+        self.step_name = step_name
+        self.context = context or {}
+
+
 class Bot:
     """
     Echo bot для vk.com
     Use Python 3.8
+    поддерживает запрос даты, места проведения и сценарий регистрации:
+    -запрос имени
+    -запрос - e-mail
+    -сообщение об успешной регистрации
+    Если шаг не пройден, новый запрос до успешного прохождения
     """
     def __init__(self, group_id, token):
         """
@@ -43,6 +58,7 @@ class Bot:
         self.vk = vk_api.VkApi(token=self.token)
         self.long_poller = VkBotLongPoll(vk=self.vk, group_id=self.group_id)
         self.api = self.vk.get_api()
+        self.user_states = dict()  # user_id : user state
 
     def run(self):
         """Запуск бота."""
@@ -58,13 +74,61 @@ class Bot:
         :param event: VkBotMessageEvent object
         :return:
         """
-        if event.type == VkBotEventType.MESSAGE_NEW:
-            log.debug('отправляем сообщение назад')
-            self.api.messages.send(message='это эхо: ' + event.object['message']['text'],
-                                   random_id=random.randint(0, 2**20),
-                                   peer_id=event.object['message']['peer_id'])
-        else:
+        if event.type != VkBotEventType.MESSAGE_NEW:
             log.info('мы пока не умеем обрабатывать такие события %s', event.type)
+            return
+        user_id = event.object['message']['peer_id']
+        text = event.object['message']['text']
+        if user_id in self.user_states:
+            text_to_send = self.continue_scenario(user_id, text)
+        else:
+            # search intent
+            for intent in settings.INTENTS:
+                if any(token in text.lower() for token in intent['tokens']):
+                    log.debug(f'User gets {intent}')
+                    if intent['answer']:
+                        text_to_send = intent['answer']
+                    else:
+                        text_to_send = self.start_scenario(user_id, intent['scenario'])
+                    break
+            else:
+                text_to_send = settings.DEFAULT_ANSWER
+
+        self.api.messages.send(
+            message=text_to_send,
+            random_id=random.randint(0, 2 ** 20),
+            peer_id=user_id
+        )
+
+    def start_scenario(self, user_id, scenario_name):
+        scenario = settings.SCENARIOS[scenario_name]
+        first_step = scenario['first_step']
+        step = scenario['steps'][first_step]
+        text_to_send = step['text']
+        self.user_states[user_id] = UserState(scenario_name=scenario_name, step_name=first_step)
+        return text_to_send
+
+    def continue_scenario(self, user_id, text):
+        state = self.user_states[user_id]
+        steps = settings.SCENARIOS[state.scenario_name]['steps']
+        #  continue scenario
+        step = steps[state.step_name]
+        handler = getattr(handlers, step['handler'])
+        if handler(text=text, context=state.context):
+            # next_step
+            next_step = step['next_step']
+            text_to_send = steps[next_step]['text'].format(**state.context)
+            if steps[next_step]['next_step']:
+                # switch to next step
+                state.step_name = step['next_step']
+            else:
+                # finish scenario
+                log.info('Зарегистрирован {name} {email}'.format(**state.context))
+                self.user_states.pop(user_id)
+        else:
+            # retry current step
+            text_to_send = step['failure_text'].format(**state.context)
+        return text_to_send
 
 
 if __name__ == '__main__':
--- chatbot/tests.py
+++ chatbot/tests.py
@@ -1,9 +1,11 @@
+from copy import deepcopy
 from unittest import TestCase
-from unittest.mock import patch, Mock, ANY
+from unittest.mock import patch, Mock
 
 from vk_api.bot_longpoll import VkBotMessageEvent
 
 from bot import Bot
+from chatbot import settings
 
 
 class Test1(TestCase):
@@ -31,43 +33,89 @@ class Test1(TestCase):
         long_poller_listen_mock.listen = long_poller_mock
         # Дублировать "with" не нужно используйте перечисление
         with patch('bot.vk_api.VkApi'), patch('bot.VkBotLongPoll', return_value=long_poller_listen_mock):
-                bot = Bot('', '')
-                bot.on_event = Mock()
-                bot.run()
+            bot = Bot('', '')
+            bot.on_event = Mock()
+            bot.run()
 
-                bot.on_event.assert_called()
-                bot.on_event.assert_any_call(obj)
-                assert bot.on_event.call_count == count
+            bot.on_event.assert_called()
+            bot.on_event.assert_any_call(obj)
+            assert bot.on_event.call_count == count
 
-    def test_on_event(self):
-        #  А остальная логика где потерялась? Также не хватает ожидаемых ответов от бота
-        #  Сначала создаем моки
-        #  - send_mock = Mock()
-        #  - api_mock = Mock()
-        #  И присваиваем отправки сообщения мок api_mock.messages.send = send_mock
-        #  .
-        #  Затем в цикле собираем список ожидаемых ответов и добавляем VkBotMessageEvent(текст)
-        #  И потом уже создаем мок для long poll, как в тесте выше, а в "return_value" передаем собранный список
-        #  .
-        #  Следующий шаг это сделать патч для VkBotLongPoll больше не нужно,
-        #  и в return_value уже передаем замоканый long poll
-        #  И внутри уже создаем бота запускаем его и добавляем ожидаемые ответы
-        #  .
-        #  И потом делаем сравнения с количество вызовов, и ожидаемы ответы с реальными
+    # def test_on_event(self):
+    #     #  А остальная логика где потерялась? Также не хватает ожидаемых ответов от бота
+    #     #  Сначала создаем моки
+    #     #  - send_mock = Mock()
+    #     #  - api_mock = Mock()
+    #     #  И присваиваем отправки сообщения мок api_mock.messages.send = send_mock
+    #     #  .
+    #     #  Затем в цикле собираем список ожидаемых ответов и добавляем VkBotMessageEvent(текст)
+    #     #  И потом уже создаем мок для long poll, как в тесте выше, а в "return_value" передаем собранный список
+    #     #  .
+    #     #  Следующий шаг это сделать патч для VkBotLongPoll больше не нужно,
+    #     #  и в return_value уже передаем замоканый long poll
+    #     #  И внутри уже создаем бота запускаем его и добавляем ожидаемые ответы
+    #     #  .
+    #     #  И потом делаем сравнения с количество вызовов, и ожидаемы ответы с реальными
+    #
+    #     # сейчас оба теста проходят. Тест на on_event сделан как показано в видео.
+    #     event = VkBotMessageEvent(raw=self.RAW_EVENT)
+    #
+    #     send_mock = Mock()
+    #     with patch('bot.vk_api.VkApi'), patch('bot.VkBotLongPoll'):
+    #             bot = Bot('', '')
+    #             bot.api = Mock()
+    #             bot.api.messages.send = send_mock
+    #
+    #             bot.on_event(event)
+    #
+    #     send_mock.assert_called_once_with(
+    #         message='это эхо: ' + self.RAW_EVENT['object']['message']['text'],
+    #         random_id=ANY,
+    #         peer_id=self.RAW_EVENT['object']['message']['peer_id']
+    #     )
 
-        # TODO сейчас оба теста проходят. Тест на on_event сделан как показано в видео.
-        event = VkBotMessageEvent(raw=self.RAW_EVENT)
+    INPUTS = [
+        'Привет',
+        'А когда?',
+        'Где будет конференция?',
+        'Зарегистрируйте меня',
+        'Вениамин',
+        'мой адрес mail@email',
+        'mail@email.ru',
+    ]
+    EXPECTED_OUTPUTS = [
+        settings.DEFAULT_ANSWER,
+        settings.INTENTS[0]['answer'],
+        settings.INTENTS[1]['answer'],
+        settings.SCENARIOS['registration']['steps']['step1']['text'],
+        settings.SCENARIOS['registration']['steps']['step2']['text'],
+        settings.SCENARIOS['registration']['steps']['step2']['failure_text'],
+        settings.SCENARIOS['registration']['steps']['step3']['text'].format(name='Вениамин', email='mail@email.ru'),
+    ]
 
+    def test_run_ok(self):
         send_mock = Mock()
-        with patch('bot.vk_api.VkApi'), patch('bot.VkBotLongPoll'):
-                bot = Bot('', '')
-                bot.api = Mock()
-                bot.api.messages.send = send_mock
+        api_mock = Mock()
+        api_mock.messages.send = send_mock
 
-                bot.on_event(event)
+        events = []
+        for input_text in self.INPUTS:
+            event = deepcopy(self.RAW_EVENT)
+            event['object']['message']['text'] = input_text
+            events.append(VkBotMessageEvent(event))
 
-        send_mock.assert_called_once_with(
-            message='это эхо: ' + self.RAW_EVENT['object']['message']['text'],
-            random_id=ANY,
-            peer_id=self.RAW_EVENT['object']['message']['peer_id']
-        )
+        long_poller_mock = Mock()
+        long_poller_mock.listen = Mock(return_value=events)
+
+        with patch('bot.VkBotLongPoll', return_value=long_poller_mock):
+            bot = Bot('', '')
+            bot.api = api_mock
+            bot.run()
+
+        assert send_mock.call_count == len(self.INPUTS)
+
+        real_outputs = []
+        for call in send_mock.call_args_list:
+            args, kwargs = call
+            real_outputs.append(kwargs['message'])
+        assert real_outputs == self.EXPECTED_OUTPUTS
