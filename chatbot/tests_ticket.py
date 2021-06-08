a1a6a134914c90afeffcf67267a6e1c8c983cf48
Mikhail <rymv@yandex.ru>
2021-06-08 18:54:46 +0300

    lesson 015 билетный бот

---
+++ chatbot/tests_ticket.py
@@ -0,0 +1,101 @@
+from copy import deepcopy
+from unittest import TestCase
+from unittest.mock import patch, Mock
+
+from vk_api.bot_longpoll import VkBotMessageEvent
+
+from ticket_bot import TicketBot
+from chatbot import ticket_settings
+
+
+class Test1(TestCase):
+    RAW_EVENT = {'type': 'message_new', 'object': {'message': {'date': 1621236521, 'from_id': 4753058,
+                                                               'id': 94, 'out': 0, 'peer_id': 4753058,
+                                                               'text': 'привет бот!!!',
+                                                               'conversation_message_id': 94, 'fwd_messages': [],
+                                                               'important': False, 'random_id': 0,
+                                                               'attachments': [], 'is_hidden': False},
+                                                   'client_info': {
+                                                       'button_actions': ['text', 'vkpay', 'open_app', 'location',
+                                                                          'open_link',
+                                                                          'callback', 'intent_subscribe',
+                                                                          'intent_unsubscribe'],
+                                                       'keyboard': True, 'inline_keyboard': True, 'carousel': False,
+                                                       'lang_id': 0}},
+                 'group_id': 204336937, 'event_id': '5e64924da9b835267578b658349f584b5ed3471e'}
+
+    def test_run(self):
+        count = 5
+        obj = {'a': 11}
+        events = [obj] * count
+        long_poller_mock = Mock(return_value=events)
+        long_poller_listen_mock = Mock()
+        long_poller_listen_mock.listen = long_poller_mock
+        with patch('ticket_bot.vk_api.VkApi'), patch('ticket_bot.VkBotLongPoll', return_value=long_poller_listen_mock):
+            bot = TicketBot('', '')
+            bot.on_event = Mock()
+            bot.run()
+
+            bot.on_event.assert_called()
+            bot.on_event.assert_any_call(obj)
+            assert bot.on_event.call_count == count
+
+    INPUTS = [
+        'Привет',
+        '/ticket',
+        'Москв',
+        'париже',
+        '12.08.2021',
+        '21-06-2021',
+        '3',
+        '2',
+        'очень нужны билеты',
+        'да',
+        '9217777777',
+    ]
+    EXPECTED_OUTPUTS = [
+        ticket_settings.HELP_ANSWER,
+        ticket_settings.SCENARIOS['booking']['steps']['step1']['text'],
+        ticket_settings.SCENARIOS['booking']['steps']['step2']['text'],
+        ticket_settings.SCENARIOS['booking']['steps']['step3']['text'],
+        ticket_settings.SCENARIOS['booking']['steps']['step3']['failure_text'],
+        ticket_settings.SCENARIOS['booking']['steps']['step4']['text'].format(flights=['1. 21-06-2021 20-40 мп02',
+                                                                                       '2. 23-06-2021 12-15 мп03',
+                                                                                       '3. 23-06-2021 18-20 мп04',
+                                                                                       '4. 23-06-2021 23-20 мп05',
+                                                                                       '5. 24-06-2021 07-20 мп06']),
+        ticket_settings.SCENARIOS['booking']['steps']['step5']['text'],
+        ticket_settings.SCENARIOS['booking']['steps']['step6']['text'],
+        ticket_settings.SCENARIOS['booking']['steps']['step7']['text'].format(departure='москва', destination='париж',
+                                                                              flight='23-06-2021 18-20 мп04', seats='2',
+                                                                              comment='очень нужны билеты'),
+        ticket_settings.SCENARIOS['booking']['steps']['step8']['text'],
+        ticket_settings.SCENARIOS['booking']['steps']['step9']['text'].format(phone='9217777777'),
+    ]
+
+    def test_run_ok(self):
+        send_mock = Mock()
+        api_mock = Mock()
+        api_mock.messages.send = send_mock
+
+        events = []
+        for input_text in self.INPUTS:
+            event = deepcopy(self.RAW_EVENT)
+            event['object']['message']['text'] = input_text
+            events.append(VkBotMessageEvent(event))
+
+        long_poller_mock = Mock()
+        long_poller_mock.listen = Mock(return_value=events)
+
+        with patch('ticket_bot.VkBotLongPoll', return_value=long_poller_mock):
+            bot = TicketBot('', '')
+            bot.api = api_mock
+            bot.run()
+
+        assert send_mock.call_count == len(self.INPUTS)
+
+        real_outputs = []
+        for call in send_mock.call_args_list:
+            args, kwargs = call
+            real_outputs.append(kwargs['message'])
+        assert real_outputs == self.EXPECTED_OUTPUTS
--- chatbot/ticket_handlers.py
+++ chatbot/ticket_handlers.py
@@ -1,32 +1,10 @@
 # -*- coding: utf-8 -*-
-# функция, которая принимает на вход text(текст входящего сообщения) и context (dict)б а возвращает bool:
+# функция, которая принимает на вход text(текст входящего сообщения) и context (dict), а возвращает bool:
 # True если шаг пройден, False если данные введены неправильно
 import datetime
 import re
 from schedule import SCHEDULE
 from ticket_settings import RE_CITIES
-
-
-# re_name = re.compile(r'^[\w\-\s]{3,40}$')
-# re_email = re.compile(r'^\b[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+\b')
-#
-#
-# def handle_name(text, context):
-#     match = re.match(re_name, text)
-#     if match:
-#         context['name'] = text
-#         return True
-#     else:
-#         return False
-#
-#
-# def handle_email(text, context):
-#     matches = re.findall(re_email, text)
-#     if len(matches) > 0:
-#         context['email'] = matches[0]
-#         return True
-#     else:
-#         return False
 
 
 def change_city_name(text):
