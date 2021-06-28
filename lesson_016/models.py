e169f2e7e7a8fdd8f9fe622435e1196be12634e6
Mikhail <rymv@yandex.ru>
2021-06-28 15:59:33 +0300

    lesson 016

--- lesson_016/models.py
+++ lesson_016/models.py
@@ -1,11 +1,20 @@
 import peewee
+from playhouse.db_url import connect
 
-database = peewee.SqliteDatabase('weather_forecast.db')
+# database = peewee.SqliteDatabase('weather_forecast.db')
+
+database_proxy = peewee.DatabaseProxy()  # Create a proxy for our db.
 
 
 class BaseTable(peewee.Model):
     class Meta:
-        database = database
+        # database = database
+        database = database_proxy
+
+
+url = 'sqlite:///weather_forecast.db'
+database = connect(url)
+database_proxy.initialize(database)
 
 
 class WeatherBase(BaseTable):
